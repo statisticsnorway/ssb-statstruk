679a2f808996f73c6f2d1733053d71d90bff0137
sjentoft <coo@ssb.no>
2024-02-21 17:39:14 +0100

    added verbose

--- src/statstrukt/ssbmodel.py
+++ src/statstrukt/ssbmodel.py
@@ -1,3 +1,5 @@
+# mypy: ignore-errors
+
 import numpy as np
 import pandas as pd
 
@@ -6,9 +8,20 @@ class ssbmodel:
     """Class for model estimation."""
 
     def __init__(
-        self, pop_data: pd.DataFrame, sample_data: pd.DataFrame, id_nr: str
+        self,
+        pop_data: pd.DataFrame,
+        sample_data: pd.DataFrame,
+        id_nr: str,
+        verbose: int = 1,
     ) -> None:
-        """Initialize general ssbmodel object."""
+        """Initialize general ssbmodel object.
+
+        Args:
+            pop_data: Population data with one row per unit (company)
+            sample_data: Sample data containing variables on the interest variable(s).
+            id_nr: Name of the variable to identify units in the population and sample
+            verbose: Whether to show printed output or not. 1 is minimal output, 2 is more output.
+        """
         self.pop_data = pop_data
         self.sample_data = sample_data
 
@@ -24,32 +37,27 @@ class ssbmodel:
             id_nr, self.sample_data, check_for_char=True, remove_missing=False
         )
         self.id_nr = id_nr
+        self.verbose = verbose
 
     def __call__(self) -> None:
         """Print model object."""
         print(
-            "strukturmodel instance with data including population size "
-            + self.pop_data.shape[0]
-            + " and sample size "
-            + self.sample_data.shape[0]
+            f"strukturmodel instance with data including population size {self.pop_data.shape[0]} and sample size {self.sample_data.shape[0]}"
         )
 
-    def _check_variable(
-        self,
-        var_name: str,
-        dataset: pd.DataFrame,
-        data_name: str = "sample",
-        check_for_char: bool = False,
-        remove_missing: bool = False,
-    ) -> None:
+    def change_verbose(self, verbose: int) -> None:
+        """Change the verbose print level."""
+        self.verbose = verbose
+
+    def _check_variable(self, var_name, dataset, data_name="sample", check_for_char=False, remove_missing=True):  # type: ignore
         """Check if the given variable name is in the dataset.
 
         Args:
-            var_name: str, the name of the variable to check.
-            dataset: dict, the dataset where keys are variable names and values are the data.
-            data_name: str,
-            check_for_char: bool, False by default. If True, checks for an ID variable that can be numeric or character.
-            remove_missing: bool, Whether to remove missing matches.
+            var_name: The name of the variable to check.
+            dataset: The dataset where keys are variable names and values are the data.
+            data_name: Name of the dataset to use in error message
+            check_for_char: False by default. If True, checks for an ID variable that can be numeric or character.
+            remove_missing: Whether to remove missing matches. Default True.
 
         Returns:
             None: The function will either pass silently or raise an error.
@@ -76,23 +84,22 @@ class ssbmodel:
             )
 
         # Check and remove observations with missing values
-        if remove_missing:
+        if (remove_missing) & (data_name == "sample"):
             cleaned_dataframe = dataset.dropna(subset=var_name)
             missing_num = dataset.shape[0] - cleaned_dataframe.shape[0]
             if missing_num > 0:
                 print(
-                    f"There were {missing_num} missing values in variable {var_name}. These observations were reomved from the sample"
+                    f"There were {missing_num} missing values in variable {var_name}. These observations were removed from the sample."
                 )
             self.sample_data = cleaned_dataframe
         else:
-
             # Give error for missing values
-            if value is None or (isinstance(value, float) and np.isnan(value)):
+            if value.isna().any():
                 raise ValueError(
-                    f"Variable '{var_name}' contains missing values. Please fix and try again."
+                    f"Variable '{var_name}' contains missing values. Please fix and try again or set remove_missing = False."
                 )
 
-    def _add_flag(self) -> None:
+    def _add_flag(self):  # type: ignore
         """Add flag in population data to say if unit is in the sample or not."""
         self.flag_var = "_flag_sample"
         sample_ids = set(self.sample_data[self.id_nr])
@@ -100,7 +107,7 @@ class ssbmodel:
             lambda x: 1 if x in sample_ids else 0
         )
 
-    def _check_model_run(self) -> None:
+    def _check_model_run(self):  # type: ignore
         """Check to ensure that model has been run before proceeding with other functions."""
         if not hasattr(self, "strata_results"):
             raise RuntimeError("Model has not been run. Please run fit() first")
