cb4db2ac18bf9cb4d43b3a6d1241e33a7ee5d7d1
sjentoft <coo@ssb.no>
2024-07-11 10:03:55 +0200

    Added basic run code again

---
+++ src/run.py
@@ -0,0 +1,9 @@
+# Code for importing library and used for working and testing code interactively
+import pandas as pd
+from statstruk import ratemodel
+
+s_data = pd.read_csv("../tests/data/sample_data.csv")
+p_data = pd.read_csv("../tests/data/pop_data.csv")
+
+
+
